ac6bcb96cb6a2d7311e74c384fd345c73308fc17
Remi Hansen <hansenrd@bc.edu>
2022-11-06 22:46:28 -0500

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Remi Pacifico Hansen on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatchEmAll/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CatchEmAll
+//
+//  Created by Remi Pacifico Hansen on 11/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
